d987c9e4895cba187ba6d839e7ff4b2a09cb9769
Yarin Levi <85311237+yarinl139@users.noreply.github.com>
2023-01-04 22:32:19 +0200

    Add files via upload

---
+++ OOPProject2Part2/Task.java
@@ -0,0 +1,41 @@
+import java.util.concurrent.Callable;
+
+public class Task<T> implements Callable<T>,Comparable<Task<T>>{
+    private Callable<T> operation;
+    public int prio;
+
+    public Task(Callable<T> operation,TaskType type)
+    {
+        this.operation = operation;
+        this.prio = type.getPriorityValue();
+    }
+    public Task(Callable<T> operation)
+    {
+        this.operation = operation;
+        this.prio = 3;
+    }
+
+    public static Task createTask(Callable operation,TaskType type)
+    {
+        return new Task(operation,type);
+    }
+    public static Task createTask(Callable operation)
+    {
+        return new Task(operation);
+    }
+
+    @Override
+    public T call() throws Exception {
+        return this.operation.call();
+    }
+
+	@Override
+	public int compareTo(Task<T> other) {
+		if(this.prio<other.prio)
+			return -1;
+		else if (this.prio>other.prio)
+			return 1;
+		return 0;
+	}
+
+}
