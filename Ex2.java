eead01939b31e51e36420b28d199361a3a49dce6
Yarin Levi <85311237+yarinl139@users.noreply.github.com>
2023-01-07 23:30:04 +0200

    Add files via upload

---
+++ Ex2.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class Ex2 {
+    public static void deleteFiles(int n)
+    {
+        for (int i = 1; i <= n; i++) {
+            String name = "file_"+i;
+            File file = new File(name);
+            file.delete();
+        }
+    }
+    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
+        String[]arr = Ex2_1.createTextFiles(1000,12566,50);
+        long startTime=0,endTime=0,totalTime=0;
+
+        startTime = System.nanoTime();
+        Ex2_1.getNumOfLines(arr);
+        endTime = System.nanoTime();
+        totalTime= endTime - startTime;
+        System.out.println("Total time for first function is: " + totalTime/1000000000.0);
+
+        Ex2_1 x = new Ex2_1();
+
+        startTime = System.nanoTime();
+        x.getNumOfLinesThreads(arr);
+        endTime = System.nanoTime();
+        totalTime = endTime - startTime;
+        System.out.println("Total time for second function is: " + totalTime/1000000000.0);
+
+        startTime = System.nanoTime();
+        x.getNumOfLinesThreadPool(arr);
+        endTime = System.nanoTime();
+        totalTime = endTime - startTime;
+        System.out.println("Total time for third function is: " + totalTime/1000000000.0);
+
+        deleteFiles(1000);
+    }
+}
---
+++ Ex2_1.java
@@ -0,0 +1,65 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+public class Ex2_1 {
+    public static String[] createTextFiles(int n,int seed,int bound) throws IOException {
+        String[] file_names = new String[n];
+        Random rnd = new Random(seed);
+        for (int i = 1; i <= n; i++) {
+            String name = "file_" + i;
+            file_names[i-1] = name;
+            File file = new File(name);
+            file.createNewFile();
+            int x = rnd.nextInt(bound);
+            FileWriter fw = new FileWriter(name);
+            for (int j = 0; j < x-1; j++) {
+                fw.write("This is a line !\n");
+            }
+            fw.write("This is a line !");
+            fw.close();
+        }
+        return file_names;
+    }
+    public static int getNumOfLines(String []fileNames) throws FileNotFoundException {
+        int count_rows=0;
+        for (int i = 0; i < fileNames.length; i++) {
+            File file = new File(fileNames[i]);
+            Scanner sc = new Scanner(file);
+            while(sc.hasNext())
+            {
+                count_rows++;
+                sc.nextLine();
+            }
+            sc.close();
+        }
+        return count_rows;
+    }
+    public int getNumOfLinesThreads(String[] fileNames) throws InterruptedException {
+        int sum = 0;
+        for (int i = 0; i < fileNames.length; i++) {
+            section_3 s3 = new section_3(fileNames[i]);
+            s3.start();
+            s3.join();
+            sum+=s3.getCount_rows();
+        }
+        return sum;
+    }
+    public int getNumOfLinesThreadPool(String[] fileNames) throws ExecutionException, InterruptedException {
+        int sum = 0;
+        ExecutorService executor = Executors.newFixedThreadPool(fileNames.length);
+        for (int i = 0; i < fileNames.length; i++) {
+            section_4 s4 = new section_4(fileNames[i]);
+            Future<Integer> future = executor.submit(s4);
+            sum+=future.get();
+        }
+        executor.shutdown();
+        return sum;
+    }
+
+
+}
